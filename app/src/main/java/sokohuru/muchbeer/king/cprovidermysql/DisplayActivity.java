cfc0bea7f07ec6c1233661aecc39fa9d31dcc5c8
muchbeer <gman2them@gmail.com>
2015-12-12 16:05:40 +0300

    hh

--- app/src/main/java/sokohuru/muchbeer/king/cprovidermysql/DisplayActivity.java
+++ app/src/main/java/sokohuru/muchbeer/king/cprovidermysql/DisplayActivity.java
@@ -1,15 +1,51 @@
 package sokohuru.muchbeer.king.cprovidermysql;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by muchbeer on 12/12/2015.
  */
-public class DisplayActivity extends AppCompatActivity{
+public class DisplayActivity extends FragmentActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    TextView resultView=null;
+    CursorLoader cursorLoader;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+        resultView= (TextView) findViewById(R.id.res);
+
+        getSupportLoaderManager().initLoader(1, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        cursorLoader= new CursorLoader(this, Uri.parse("content://sokohuru.muchbeer.king.cprovidermysql.MyProvider/cte"), null, null, null, null);
+        return cursorLoader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        cursor.moveToFirst();
+        StringBuilder res=new StringBuilder();
+        while (!cursor.isAfterLast()) {
+            res.append("\n"+cursor.getString(cursor.getColumnIndex("id"))+ "-"+ cursor.getString(cursor.getColumnIndex("name")));
+            cursor.moveToNext();
+        }
+        resultView.setText(res);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
